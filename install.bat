a66f4c16679fc98acd3ac8deb3faeb19ceea949c
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-09 09:44:05 +0200

    fix: python version selection in install script

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-python -3.7 -m venv venv
+py -3.7 -m venv venv
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe -m pip install PyAudio-0.2.11-cp37-cp37m-win_amd64.whl
 pause
