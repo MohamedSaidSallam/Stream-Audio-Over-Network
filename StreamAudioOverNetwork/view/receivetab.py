9afe044f0f07a075b5d4ed431a0c5b5616a33f50
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-02-09 09:37:04 +0200

    feat: my local ip label in receive tab

--- StreamAudioOverNetwork/view/receivetab.py
+++ StreamAudioOverNetwork/view/receivetab.py
@@ -1,21 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
-from StreamAudioOverNetwork.view.utility import setGrid
-from StreamAudioOverNetwork.streamaudio import ReceiveAudio
 from StreamAudioOverNetwork.firewall import toggleFirewall
+from StreamAudioOverNetwork.network.networkutility import getLocalIP
+from StreamAudioOverNetwork.streamaudio import ReceiveAudio
+from StreamAudioOverNetwork.view.utility import setGrid
 
 
 def getReceiveTab(notebook):
     receiveAudio = ReceiveAudio()
     receiveTab = ttk.Frame(notebook)
 
+    myIPLabel = tk.Label(receiveTab, text=f"My Local IP: {getLocalIP()}")
+    setGrid(myIPLabel, row=0)
+
     portLabel = tk.Label(receiveTab, text="Port: ")
     portTextBox = tk.Entry(receiveTab)
     portTextBox.insert(0, '5000')
 
-    setGrid(portLabel, row=0, colSpan=1)
-    setGrid(portTextBox, col=1, row=0, colSpan=1)
+    setGrid(portLabel, row=1, colSpan=1)
+    setGrid(portTextBox, col=1, row=1, colSpan=1)
 
     def toggleReceiving():
         if receiveAudio.isStopped():
@@ -30,11 +34,11 @@ def getReceiveTab(notebook):
                                       text="Start Receiving",
                                       command=toggleReceiving
                                       )
-    setGrid(toggleReceivingButton, row=1)
+    setGrid(toggleReceivingButton, row=2)
     toggleFirewallgButton = tk.Button(receiveTab,
                                       text="Toggle Firewall",
                                       command=toggleFirewall
                                       )
-    setGrid(toggleFirewallgButton, row=2)
+    setGrid(toggleFirewallgButton, row=3)
 
     return receiveTab
